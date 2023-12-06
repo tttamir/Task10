75962c130f3ee5647313883dba95e4eed1d6650a
tttamir <tamircook1003@gmail.com>
2023-12-06 18:30:18 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        User user1 = new User(20, "3ttt");
+        User user2 = new User(20, "3ttt");
+        System.out.println(user1.equals(user2));
+
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,37 @@
+import java.util.Objects;
+
+public class User {
+    int age;
+    String name;
+
+    public User(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (age != user.age) return false;
+        return Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = age;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+}
